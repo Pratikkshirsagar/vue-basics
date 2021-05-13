77fabd0687ac064f1c651d84c46ef7e7efd6cbbf
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-05-13 23:13:18 +0530

    mouse events

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ const app = Vue.createApp({
         showBooks: true,
         title: 'The Final Empire',
         auther: 'Brandon Sanderson',
-        age: 45   
+        age: 45,   
+        x: 0,
+        y: 0
+
       }
   },
   methods: {
     toggleShowBooks(title) {
         this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data) {
+      console.log('event', data);
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
+
   }
 });
 
