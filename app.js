64f14ea18795fa7cdea092bb121a714b7aee562b
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2021-05-13 23:25:17 +0530

    v for

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const app = Vue.createApp({
         auther: 'Brandon Sanderson',
         age: 45,   
         x: 0,
-        y: 0
-
+        y: 0,
+        books: [
+          { title: 'name of the wind', author: 'patrick rothfuss' },
+          { title: 'the way of kings', author: 'brandon sanderson' },
+          { title: 'the final empire', author: 'brandon sanderson' }
+        ]
       }
   },
   methods: {
